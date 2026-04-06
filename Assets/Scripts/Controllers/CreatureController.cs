8b9d5879a3ba7b9db58eabb24c96a5e3997358fc
JUNG HOO CHOI <chl9430@gmail.com>
2026-04-07 02:16:05 +0900

    Initial commit.

---
+++ Assets/Scripts/Controllers/CreatureController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using UnityEngine;
+using static Define;
+
+public class CreatureController : BaseController
+{
+	protected override void Init()
+	{
+		base.Init();
+	}
+
+	public virtual void OnDamaged()
+	{
+	}
+
+	public virtual void OnDead()
+	{
+	}
+
+	public virtual void UseSkill(int skillId)
+	{
+	}
+}
---
+++ Assets/Scripts/Controllers/MyPlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayerController : CreatureController
+{
+	protected override void Init()
+	{
+		base.Init();
+	}
+
+	protected override void UpdateController()
+	{
+		GetUIKeyInput();
+
+		base.UpdateController();
+	}
+
+	protected override void UpdateIdle()
+	{
+	}
+
+	void LateUpdate()
+	{
+		Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+	}
+
+	void GetUIKeyInput()
+	{
+		if (Input.GetKeyDown(KeyCode.I))
+		{
+		}
+		else if (Input.GetKeyDown(KeyCode.C))
+		{
+		}
+	}
+
+	// 키보드 입력
+	void GetDirInput()
+	{
+		if (Input.GetKey(KeyCode.W))
+		{
+		}
+		else if (Input.GetKey(KeyCode.S))
+		{
+		}
+		else if (Input.GetKey(KeyCode.A))
+		{
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+		}
+	}
+}
